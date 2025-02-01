365499e3585fa2299834c1eb8063c14dbdf3256c
WeihongQ <34804641+WeihongQ@users.noreply.github.com>
2025-02-01 17:53:17 +0000

    Deploying to gh-pages from @ WeihongQ/weihongq.github.io@db1b19b6311e3e1ea5ff2a4739c2e5b092ca79fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,16 +30,17 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+        },{id: "news-in-our-new-studies-icwsm-2024-icwsm-2023-we-uncover-media-bias-in-1-8-million-headlines-across-the-political-spectrum-our-findings-suggest-that-media-bias-is-growing-the-work-is-also-covered-by-studyfinds-and-shared-by-scienceblog-realclearscience-nation-mirage-news-phys-org-the-horizons-tracker-and-the-american-association-for-the-advancement-of-science-aaas-via-eurekalert",
+          title: 'In our new studies (ICWSM 2024, ICWSM 2023), we uncover media bias in...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-our-work-on-the-discrepancy-in-emoji-understanding-and-usage-between-lmms-and-humans-is-accepted-at-icwsm-24",
+          title: 'Our work on the discrepancy in emoji understanding and usage between LMMs and...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-am-honored-to-be-selected-to-receive-the-travel-award-from-the-diversity-community-of-the-society-for-political-methodology-i-will-be-attending-polmeth-24-in-riverside-california-from-july-18-to-20",
+          title: 'I am honored to be selected to receive the travel award from the...',
+          description: "",
+          section: "News",},{id: "news-our-latest-study-telematics-and-informatics-analyzes-public-opinions-on-emerging-artificial-intelligence-technologies-this-work-has-been-featured-by-media-outlets-including-tech-xplore-quantum-zeitgeist-and-digital-information-world",
+          title: 'Our latest study (Telematics and Informatics) analyzes public opinions on emerging artificial intelligence...',
           description: "",
           section: "News",},{
         id: 'social-email',
